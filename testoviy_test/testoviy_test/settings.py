588f78ec83a100a927a2068f4833ccca405e272b
GrozaMorey <yorik294@gmail.com>
2022-12-09 01:10:11 +0300

    rebuild app

--- testoviy_test/testoviy_test/settings.py
+++ testoviy_test/testoviy_test/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'testoviy_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "django1",
+        'USER': "postgres",
+        'PASSWORD': "123",
+        'HOST': "localhost",
+        'PORT': "5432"
     }
 }
 
